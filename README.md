1cc2637377ed41a8af1afe53bd141ffa52551edd
Wai Yan Sze <szewaiyan@Wais-MacBook-Air.local>
2025-04-15 16:57:10 +0100

    Just finished inserting topics

--- README.md
+++ README.md
@@ -1,3 +1,35 @@
 # NC News Seeding
 
-- Instructions for this sprint can be found at https://l2c.northcoders.com/courses/be/seeding-nc-news
+this is a Back End project: creating and seeding a test and development database with articles, users, comments and topics.
+
+# Project Setup
+
+git clone https://github.com/snorlaxcwy/my-seeding-stuff.git
+
+# Install dependencies (package.json)
+
+npm insatll
+=> this is including
+pg - PostegreSQL for Node.js
+dotenv - for managaing environment variables
+pg-format - for formatting safe SQL queries in the seed script
+jest - for testing
+
+# Create environment variable files
+
+.env.development
+.env.test
+=> need to put them into the root folder but not db
+
+# Create Database
+
+npm run setup-dbs
+
+# create table
+
+- dropTables()
+- createTables()
+
+and then test createTables by using db.query
+
+npm run test-seed to test each codings
--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -1,6 +1,138 @@
-const db = require("../connection")
-
+const db = require("../connection");
+const format = require("pg-format");
+/*
+//Drop the tables first
 const seed = ({ topicData, userData, articleData, commentData }) => {
-  return db.query(); //<< write your first query in here.
+  return (
+    db
+      .query(`DROP TABLE IF EXISTS comments;`)
+      .then(() => db.query(`DROP TABLE IF EXISTS articles;`))
+      .then(() => db.query(`DROP TABLE IF EXISTS users;`))
+      .then(() => db.query(`DROP TABLE IF EXISTS topics;`))
+
+      //Create the tables
+      .then(() => {
+        return db.query(`
+        CREATE TABLE topics (
+        slug VARCHAR PRIMARY KEY,
+        description VARCHAR(500) NOT NULL,
+        img_url VARCHAR(1000)
+         );
+      `);
+      })
+
+      .then(() => {
+        return db.query(`
+         CREATE TABLE users (
+         username VARCHAR(50) PRIMARY KEY,
+         name VARCHAR(80) NOT NULL,
+         avatar_url VARCHAR(1000)
+         );
+      `);
+      })
+
+      .then(() => {
+        return db.query(`
+         CREATE TABLE articles (
+         article_id SERIAL PRIMARY KEY,
+         title VARCHAR(50) NOT NULL,
+         topic VARCHAR REFERENCES topics(slug),
+         author VARCHAR REFERENCES users(username),
+         body TEXT NOT NULL,
+         created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+         votes INT DEFAULT 0,
+         article_img_url VARCHAR(1000)
+         );
+        `);
+      })
+
+      .then(() => {
+        return db.query(`
+         CREATE TABLE comments (
+         comment_id SERIAL PRIMARY KEY,
+         article_id INT REFERENCES articles(article_id),
+         body TEXT NOT NULL,
+         votes INT DEFAULT 0,
+         author VARCHAR REFERENCES users(username),
+         created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+         );
+      `);
+      })
+  );
 };
+*/
+
+function seed({ topicData, userData, articleData, commentData }) {
+  // <-this is what we received, array of object, we could see from topic.js
+  return (
+    db
+      // Drop tables
+      .query("DROP TABLE IF EXISTS comments;")
+      .then(() => db.query("DROP TABLE IF EXISTS articles;"))
+      .then(() => db.query("DROP TABLE IF EXISTS users;"))
+      .then(() => db.query("DROP TABLE IF EXISTS topics;"))
+
+      .then(() => {
+        // Create tables
+        return db.query(`
+        CREATE TABLE topics (
+          slug VARCHAR PRIMARY KEY,
+          description VARCHAR(500) NOT NULL,
+          img_url VARCHAR(1000)
+        );
+      `);
+      })
+
+      .then(() => {
+        return db.query(`
+        CREATE TABLE users (
+          username VARCHAR(50) PRIMARY KEY,
+          name VARCHAR(80) NOT NULL,
+          avatar_url VARCHAR(1000)
+        );
+      `);
+      })
+
+      .then(() => {
+        return db.query(`
+        CREATE TABLE articles (
+          article_id SERIAL PRIMARY KEY,
+          title VARCHAR(50) NOT NULL,
+          topic VARCHAR REFERENCES topics(slug),
+          author VARCHAR REFERENCES users(username),
+          body TEXT NOT NULL,
+          created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+          votes INT DEFAULT 0,
+          article_img_url VARCHAR(1000)
+        );
+      `);
+      })
+
+      .then(() => {
+        return db.query(`
+        CREATE TABLE comments (
+          comment_id SERIAL PRIMARY KEY,
+          article_id INT REFERENCES articles(article_id),
+          body TEXT NOT NULL,
+          votes INT DEFAULT 0,
+          author VARCHAR REFERENCES users(username),
+          created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        );
+      `);
+      })
+
+      //insert topic data
+      .then(() => {
+        const formattedTopics = topicData.map(
+          // turning objects into array in array
+          ({ slug, description, img_url }) => [slug, description, img_url]
+        );
+        const insertTopicsQuery = format(
+          `INSERT INTO topics (slug, description, img_url) VALUES %L RETURNING *;`,
+          formattedTopics
+        );
+        return db.query(insertTopicsQuery);
+      })
+  );
+}
 module.exports = seed;
