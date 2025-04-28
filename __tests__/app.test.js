49ab9232827362ff955b639803297dc023cfa0e3
Wai Yan Sze <szewaiyan@Wais-MacBook-Air.local>
2025-04-28 12:38:34 +0100

    finished Task 1 get API

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,10 +1,12 @@
+const request = require("supertest");
+const app = require("../app");
 const endpointsJson = require("../endpoints.json");
 /* Set up your test imports here */
 
 /* Set up your beforeEach & afterAll functions here */
 
 describe("GET /api", () => {
-  test.skip("200: Responds with an object detailing the documentation for each endpoint", () => {
+  test("200: Responds with an object detailing the documentation for each endpoint", () => {
     return request(app)
       .get("/api")
       .expect(200)
