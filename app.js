e1a74d807520f3360ae2b8cf5e48a69dcc4a7ec9
Wai Yan Sze <szewaiyan@Wais-MacBook-Air.local>
2025-04-28 11:56:46 +0100

    fixed controller and app

--- app.js
+++ app.js
@@ -2,15 +2,13 @@
 const express = require("express");
 const app = express();
 
-const endpoints = require("./endpoints.json");
-
 //middleware
 app.use(express.json());
 
 //routes
-app.get("/api", (req, res) => {
-  res.status(200).send({ endpoints });
-});
+const { getApi } = require("./controllers/api.controller");
+
+app.get("/api", getApi);
 
 // 404 handler
 app.all("/*splat", (req, res) => {
@@ -22,7 +20,7 @@ app.use((err, req, res, next) => {
   if (err.code === "22P02") {
     return res.status(400).send({ msg: "Bad Request" });
   } else if (err.status) {
-    return res.status.send({ msg: "err.msg" });
+    return res.status(err.status).send({ msg: err.msg });
   } else {
     return res.status(500).send({ msg: "500 Internal Server Error" });
   }
