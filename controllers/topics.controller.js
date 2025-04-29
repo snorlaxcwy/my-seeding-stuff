88c71a42cf76ca2a8836f4c7ceaee0e5b2b23606
Wai Yan Sze <szewaiyan@Wais-MacBook-Air.local>
2025-04-29 14:42:28 +0100

    Completed Task 3 Get article by article_id

--- controllers/topics.controller.js
+++ controllers/topics.controller.js
@@ -1,4 +1,4 @@
-const { selectTopics } = require("../models/topics.models");
+const { selectTopics } = require("../models/topics.model");
 
 exports.getTopics = (req, res, next) => {
   selectTopics()
