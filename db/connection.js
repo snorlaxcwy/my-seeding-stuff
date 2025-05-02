3f1b82def699dd229c50541965158e709547b6be
Wai Yan Sze <szewaiyan@Wais-MacBook-Air.local>
2025-05-02 10:13:28 +0100

    setup Hosting with Supabase and Render

--- db/connection.js
+++ db/connection.js
@@ -1,16 +1,30 @@
 const { Pool } = require("pg");
 
-const ENV = process.env.NODE_ENV || 'development'
+const ENV = process.env.NODE_ENV || "development";
 
-require('dotenv').config({path: `${__dirname}/../.env.${ENV}`})
+require("dotenv").config({ path: `${__dirname}/../.env.${ENV}` });
 
-const db = new Pool();
-
-if (!process.env.PGDATABASE) {
-    throw new Error("No PGDATABASE configured")
-} else { 
-    console.log(`Connected to ${process.env.PGDATABASE}`)
+//initialize an empty config object
+const config = {};
+//connect to supabase if it's production env
+if (ENV === "production") {
+  if (!process.env.DATABASE_URL) {
+    throw new Error("DATABASE_URL not set for production");
+  }
+  config.connectionString = process.env.DATABASE_URL;
+  config.max = 2;
 }
 
+// connect to database
+const db = new Pool(config);
+
+// to check dev/test need to have PGDATABASE; Production need to have URL
+if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
+  throw new Error("PGDATABASE or DATABASE_URL not set");
+}
+
+if (ENV !== "production") {
+  console.log(`Connected to ${process.env.PGDATABASE}`);
+}
 
 module.exports = db;
